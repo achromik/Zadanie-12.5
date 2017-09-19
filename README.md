4c9f08b19bee37511e7743a2e490615259fef4fa
Aleksander Chromik <achromik@gmail.com>
2017-09-20 01:41:35 +0200

    Finished

---
+++ README.md
@@ -0,0 +1,4 @@
+# Zadanie-12.5
+
+
+#### Zadanie 12.5 z kodilla.com AJAX whit jQuery - JOKES
---
+++ js/script.js
@@ -0,0 +1,48 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var tweetLink = 'https://twitter.com/intent/tweet?text=',
+    quoteUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+
+// $.ajax({
+//     dataType: 'json',
+//     url: quoteUrl,
+//     data: null,
+//     success: createTweet
+// });
+//
+// above is alternate to code below
+//
+function getQuote() {
+    $.ajaxSetup({ cache: false });
+    $.getJSON(prefix + quoteUrl, createTweet);
+}
+
+function createTweet(input) {
+    var data = input[0];
+
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = $(data.title);
+
+    if(!quoteAuthor.length) { 
+        quoteAuthor = 'Unknown';
+    }
+
+    var tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text(quoteAuthor);
+        $('.tweet').attr('href', tweet);
+    }
+}
+
+
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').on('click', function() {
+        getQuote();
+    });
+});
+
